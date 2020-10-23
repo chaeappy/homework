89ae66dd4a17bbf3c49177bb11314c4cfaf65376
chaeappy <cheappy@gmail.com>
2020-10-23 23:03:22 +0900

    replace로 불필요한 문자열 제거

--- src/VO/Friends/Friends2.java
+++ src/VO/Friends/Friends2.java
@@ -6,12 +6,17 @@ import java.io.IOException;
 
 public class Friends2 {
 
+
 	public static void main(String[] args) throws IOException {
 		FileReader fr = new FileReader("friends.txt");
 		BufferedReader br = new BufferedReader(fr);
 		
 		String line = "";
 		while ((line = br.readLine()) != null) {
+			line = line.replace("[name:", "");
+			line = line.replace("birthDay:", "");
+			line = line.replace("]", "");
+			
 			System.out.println(line);
 		}
 	}
