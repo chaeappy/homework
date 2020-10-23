19410789aa7d3ba7eb49b527e48ecedb9ad9ed02
chaeappy <cheappy@gmail.com>
2020-10-23 22:43:06 +0900

    file로 부터 문자열로 읽어오기 성공

--- src/VO/Friends/Friends.java
+++ src/VO/Friends/Friends.java
@@ -22,14 +22,11 @@ class Friends {
 		Friends fr2 = new Friends("이묘찬", "9월3일");
 		String fileName = "\test.txt";	
 		HashMap<String, Friends> map = new HashMap();
-//		String[] strArr = new String[map.size()];
-		String str = "";
 	
 		map.put(fr.name, fr);
 		map.put(fr2.name, fr2);
 		
 		for (Entry<String, Friends> entry : map.entrySet()) {
-			String key = entry.getKey();
 			Friends value = entry.getValue();
 			bw.write(value + "\n");
 			bw.flush();
--- src/VO/Friends/Friends2.java
+++ src/VO/Friends/Friends2.java
@@ -1,16 +1,18 @@
 package VO.Friends;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class Friends2 {
 
 	public static void main(String[] args) throws IOException {
-		BufferedWriter bw = new BufferedWriter(new FileWriter("friends.txt"));
-		String str = "아아 테스트중입니다.";
-		bw.write(str);
-		bw.close();
+		FileReader fr = new FileReader("friends.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		String line = "";
+		while ((line = br.readLine()) != null) {
+			System.out.println(line);
+		}
 	}
-
 }
