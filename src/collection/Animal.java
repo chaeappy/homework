cacbc34c9a083c8516b82590fce89a2810559bf2
chaeappy <cheappy@gmail.com>
2020-10-25 01:26:25 +0900

    hashMap 동물정리 --> 출력 bufferedWriter로 바꿀 예정

---
+++ src/collection/Animal.java
@@ -0,0 +1,39 @@
+package collection;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class Animal {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("hash_test.txt"));
+		HashMap<String, Integer> map = new HashMap();
+		String line = "";
+		int value = 0;
+		int newValue = 0;
+		
+		while((line = br.readLine()) != null) {
+			String[] strArr = line.split("\\|");
+			if (map.containsKey(strArr[0])) {
+				value = map.get(strArr[0]);
+				newValue = value + Integer.parseInt(strArr[1]);
+				map.put(strArr[0], newValue);
+			} else {
+				map.put(strArr[0], Integer.parseInt(strArr[1]));
+			}
+		}
+		
+		for (Entry<String, Integer> entry : map.entrySet()) {
+			System.out.println(entry.getKey() + " : " + entry.getValue());
+			
+		}
+		System.out.println("[결과 : 총 " + map.size() + " 가지의 동물이 있습니다.]");
+		
+	}
+
+}
