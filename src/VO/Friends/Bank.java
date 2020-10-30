9d8da0768333522fd0f233df6415d1870d8476f1
chaeappy <cheappy@gmail.com>
2020-10-30 20:03:40 +0900

    사용자 삭제기능 이름 등록시 왜 엔터를 2번 쳐야할까?

--- src/VO/Friends/Bank.java
+++ src/VO/Friends/Bank.java
@@ -29,13 +29,14 @@ public class Bank {
 		System.out.println("1. 등록 / 2. 삭제");
 		
 		if (sc.nextInt() == 1) {
-			System.out.println("이름과 아이디를 입력하세요. ex) 이름 / 아이디 / 전재산");
+			System.out.println("이름과 아이디를 입력하세요. ex) 이름 / 아이디");
 			String str = br.readLine();
 			String[] userArr = str.split("/");
 			Person ps = new Person(userArr[0], userArr[1]);
 		} else {
 			System.out.println("삭제할 이름을 입력하세요.");
 			userMap.remove(br.readLine());
+			System.out.println(userMap.size());
 		}
 			
 		
@@ -49,6 +50,7 @@ public class Bank {
 		do {
 			System.out.println("[ 1 ] 사용자 등록 / 삭제");
 			System.out.println("[ 2 ] 로그인 / 로그아웃");
+			System.out.println("[ 3 ] 입금 / 출금 / 송금 / 잔액조회");
 			input = bk.sc.nextInt();
 			
 			switch(input) {
