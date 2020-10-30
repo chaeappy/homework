c8737ab6b835e58124c03af93eb307c0d5318dbf
chaeappy <cheappy@gmail.com>
2020-10-30 21:12:22 +0900

    은행프로그램 로그인 기능까지 만듦

--- src/VO/Friends/Bank.java
+++ src/VO/Friends/Bank.java
@@ -6,6 +6,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 
@@ -32,7 +33,7 @@ public class Bank {
 			System.out.println("이름과 아이디를 입력하세요. ex) 이름 / 아이디");
 			String str = br.readLine();
 			String[] userArr = str.split("/");
-			Person ps = new Person(userArr[0], userArr[1]);
+			userMap.put(userArr[0], userArr[1]);
 		} else {
 			System.out.println("삭제할 이름을 입력하세요.");
 			userMap.remove(br.readLine());
@@ -42,15 +43,33 @@ public class Bank {
 		
 	}
 	
+	void login() throws IOException {
+		System.out.println("아이디를 입력하세요");
+		String nameGet = br.readLine();
+		Loop1 : for(Map.Entry<String, String> entry : userMap.entrySet()) {
+			String getKey = entry.getKey();
+			if (userMap.get(getKey).equals(nameGet)) {
+				String loginName[] = getKey.split("");
+				System.out.println(loginName[0] + "*" + loginName[2] + "님 환영합니다.");
+				break Loop1;
+			}
+		}
+		function();
+	}
+	
+	void function() {
+		
+	}
+	
 	public static void main(String[] args) throws IOException {
 		BufferedWriter bw = new BufferedWriter(new FileWriter("back.txt"));
 		Bank bk = new Bank();
 		int input;
+		String line = "-----------------------------------";
 		System.out.println("- - - - Bank Program - - - -");
 		do {
 			System.out.println("[ 1 ] 사용자 등록 / 삭제");
-			System.out.println("[ 2 ] 로그인 / 로그아웃");
-			System.out.println("[ 3 ] 입금 / 출금 / 송금 / 잔액조회");
+			System.out.println("[ 2 ] 로그인");
 			input = bk.sc.nextInt();
 			
 			switch(input) {
@@ -60,17 +79,18 @@ public class Bank {
 				break;
 				//사용자 로그인 로그아웃 : 해쉬맵 
 			case 2:
+				if (bk.userMap.size() != 0) {
+					bk.login();
+				} else if (bk.userMap.size() == 0) {
+					System.out.println(line);
+					System.out.println("사용자 등록후 로그인하시기 바랍니다");
+					System.out.println(line);
+				}
 				break;
-				//입금/출금
-			case 3:
-				break;
-				// 송금
-			case 4:
-				break;
-				// 잔액조
-			case 5:
-				break;
-			case 6:
+			default :
+				System.out.println(line);
+				System.out.println("다시 입력하시기 바랍니다.");
+				System.out.println(line);
 				break;
 			}
 		} while (input != 6);
