48113134bcbfc304d8623c4f06166abe54a9ebc6
chaeappy <cheappy@gmail.com>
2020-11-14 02:04:32 +0900

    cafe패키지, 클래스 cafe, customer, main, drink 나누어서 작업중

--- src/VO/cafe/Customer.java
+++ src/VO/cafe/Customer.java
@@ -1,9 +1,29 @@
 package vo.cafe;
 
+import java.util.ArrayList;
+
 public class Customer {
-	int uniqueNumber;
-	boolean gender;
+	ArrayList<Drink> list = new ArrayList<Drink>();
+	String id;
+	int gender;
+	
+	public Customer(ArrayList<Drink> list, String id, int gender) {
+		super();
+		this.list = list;
+		this.id = id;
+		this.gender = gender;
+	}
+
+	public Customer(String id, int gender) {
+		super();
+		this.id = id;
+		this.gender = gender;
+	}
+	
+
+	
 	//구매내역(현금 or 카드 / 구매한 음료 및 총 개수
+	//영수증 (음료/잔수/금액/카드or현금)
 	
 	
 	
